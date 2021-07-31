ca1232aee8a53c3ca3485052fa895459b6ab83f4
SIDDHANT DEVIL <83235714+unknownforall1@users.noreply.github.com>
2021-07-31 18:27:11 +0530

    Update siddhant.py

--- siddhant.py
+++ siddhant.py
@@ -1459,7 +1459,7 @@ async def alive(e):
     if e.sender_id in SMEX_USERS:
         text = "THODA RUKO!"
         event = await e.reply(text, parse_mode=None, link_preview=None )
-        await event.edit(f"https://telegra.ph/file/264a11a7e6c2b8a8f0277.mp4","👑𝐌𝐀𝐒𝐓𝐄𝐑👑『😈[{ALIVE_NAME}](https://t.me/{USERNAME})😈 』")
+        await event.edit(f"https://telegra.ph/file/264a11a7e6c2b8a8f0277.mp4","👑𝐌𝐀𝐒𝐓𝐄𝐑👑『😈[{ALIVE_NAME}](https://t.me/{USERNAME})😈 』","[🔥𝐑𝐄𝐏𝐎🔥](https://t.me/FATHER_SID_OP_SPAM_BOT)")
 
 
 
